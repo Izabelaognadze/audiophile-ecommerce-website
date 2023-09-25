5cf85fb63975b0d0254841ac53b1c8b5fdb5a472
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-25 13:01:45 +0400

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component'
+import {HeadphonesComponent} from "./components/headphones/headphones.component"
+import {EarphonesComponent} from './components/earphones/earphones.component'
+import {MainPageComponent} from './components/main-page/main-page.component'
+import {SpeakersComponent} from './components/speakers/speakers.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"/", component: MainPageComponent
+  },
+  {
+    path:"/home",component:HomeComponent
+  },
+  {
+    path:"/headphones",component:HeadphonesComponent
+  },
+  {
+    path:"/earphones",component:EarphonesComponent
+  },
+  {
+    path:"/speakers",component:SpeakersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeadphonesComponent } from './components/headphones/headphones.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
+import { EarphonesComponent } from './components/earphones/earphones.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadphonesComponent,
+    SpeakersComponent,
+    EarphonesComponent,
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
