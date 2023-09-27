13c6d8c59984675cba0f405523b896271749cf77
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-27 22:29:57 +0400

    header update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'clrDarkOrange':'#D87D4A',
+        'clrLightOrange':'#fbaf85',
+        'clrLightBlack':'#101010',
+        'clrDarkGrey':'#F1F1F1',
+        'clrLightGrey':'#FAFAFA',
+        'clrBg':'#0E0E0E'
+      }
+    },
   },
   plugins: [],
 }
