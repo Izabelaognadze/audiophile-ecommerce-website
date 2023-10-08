be1d82afc3c293b17763e401dcbcdce0dcfb1ff4
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-10-08 13:09:30 +0400

    best gest component

---
+++ src/app/design-system/best-gear/best-gear.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-gear',
+  templateUrl: './best-gear.component.html',
+  styleUrls: ['./best-gear.component.css']
+})
+export class BestGearComponent {
+
+}
--- src/app/design-system/design-system.module.ts
+++ src/app/design-system/design-system.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeadlineComponent } from './headline/headline.component';
 import { ShopButtonComponent } from './shop-button/shop-button.component';
+import { BestGearComponent } from './best-gear/best-gear.component';
 
 @NgModule({
   declarations: [
     HeadlineComponent,
-    ShopButtonComponent
+    ShopButtonComponent,
+    BestGearComponent
   ],
   imports: [
     CommonModule
@@ -14,6 +16,8 @@ import { ShopButtonComponent } from './shop-button/shop-button.component';
   exports:[
     HeadlineComponent,
     ShopButtonComponent,
+    BestGearComponent,
+    
   ]
 })
 export class DesignSystemModule { }
