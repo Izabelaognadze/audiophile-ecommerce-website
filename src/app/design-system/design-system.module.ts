78d8d1a1d037c29a593cdc15816969946d471e74
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-10-08 19:09:04 +0400

    category items

--- src/app/design-system/design-system.module.ts
+++ src/app/design-system/design-system.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeadlineComponent } from './headline/headline.component';
 import { ShopButtonComponent } from './shop-button/shop-button.component';
 import { BestGearComponent } from './best-gear/best-gear.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
     HeadlineComponent,
     ShopButtonComponent,
-    BestGearComponent
+    BestGearComponent,
+    ItemsComponent
   ],
   imports: [
     CommonModule
@@ -17,7 +19,7 @@ import { BestGearComponent } from './best-gear/best-gear.component';
     HeadlineComponent,
     ShopButtonComponent,
     BestGearComponent,
-    
+    ItemsComponent,
   ]
 })
 export class DesignSystemModule { }
