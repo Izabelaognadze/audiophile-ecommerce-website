69a36c81d96e38996cd7f57dc62d680b10c9c153
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-10-12 03:27:57 +0400

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import {HeadphonesComponent} from "./components/headphones/headphones.component"
 import {EarphonesComponent} from './components/earphones/earphones.component'
 import {SpeakersComponent} from './components/speakers/speakers.component'
 import {Xx99MarkTwoHeadphonesComponent} from './components/headphones/xx99-mark-two-headphones/xx99-mark-two-headphones.component'
+import {Xx99MarkOneHeadphonesComponent} from './components/headphones/xx99-mark-one-headphones/xx99-mark-one-headphones.component'
+import {Xx59HeadphonesComponent} from './components/headphones/xx59-headphones/xx59-headphones.component'
+import {Zx7SpeakerComponent} from './components/speakers/zx7-speaker/zx7-speaker.component'
+import {Zx9SpeakerComponent} from './components/speakers/zx9-speaker/zx9-speaker.component'
+import {Yx1EarphonesComponent} from './components/earphones/yx1-earphones/yx1-earphones.component'
 
 const routes: Routes = [
   {
@@ -17,10 +22,25 @@ const routes: Routes = [
         path:"headphones/xx99-mark-two-headphones",component:Xx99MarkTwoHeadphonesComponent
       },
       {
+        path:"headphones/xx99-mark-one-headphones",component:Xx99MarkOneHeadphonesComponent
+      },
+      {
+        path:"headphones/xx59-headphones",component:Xx59HeadphonesComponent
+      },
+      {
         path:"headphones",component:HeadphonesComponent
       },
       {
+        path:"earphones/yx1-earphones",component:Yx1EarphonesComponent
+      },
+      {
         path:"earphones",component:EarphonesComponent
+      },
+      {
+        path:"speakers/zx9-speaker",component:Zx9SpeakerComponent
+      },
+      {
+        path:"speakers/zx7-speaker",component:Zx7SpeakerComponent
       },
       {
         path:"speakers",component:SpeakersComponent
