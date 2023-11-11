43eb38ad9f90b4919a9582630e05df419be0e653
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-11-11 23:23:38 +0400

    card toggle component update

--- src/app/design-system/card-toggle/card-toggle.component.ts
+++ src/app/design-system/card-toggle/card-toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ItemsToBuy } from 'src/app/items-to-buy';
 
 @Component({
@@ -8,14 +8,45 @@ import { ItemsToBuy } from 'src/app/items-to-buy';
 })
 export class CardToggleComponent implements OnInit{
   cartItems : ItemsToBuy[]=[];
+  @Input() public size = 0;
+  @Output() public changedSize = new EventEmitter<number>;
+  outputMessage:number = 1;
+
+  dec() {
+    this.resize(-1)
+  }
+
+  inc() {
+    this.resize(+1)
+  }
+
+  resize(delta:number){
+    this.size = Math.min(100, Math.max(0, + this.size + delta));
+    this.changedSize.emit(this.size);
+  }
+
+  removeAll(){
+    this.cartItems = []
+  }
+
+  getTotal(): number{
+    return this.cartItems.reduce((sum, item) => 
+    sum + item.price * item.amount, 0)
+  }
 
   constructor(){
     this.cartItems = [
       {
-        image : "url('assets/cart/image-xx59-headphones.jpg')",
-        header : "XX99 MK II",
+        image : "assets/product-xx59-headphones/desktop/image-product.jpg",
+        header : "XX59 MK II",
         price: 2999,
         amount : 1
+      },
+      {
+        image : "assets/product-xx59-headphones/desktop/image-product.jpg",
+        header : "XX99 MK II",
+        price: 1000,
+        amount : 2
       },
     ]
   }
---
+++ src/app/items.ts
@@ -0,0 +1,6 @@
+export interface Items {
+    image:string,
+    header:string,
+    price:number,
+    amout:number
+}
