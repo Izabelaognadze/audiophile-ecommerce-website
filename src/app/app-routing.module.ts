09eda1a613d2f93ff43b33ade12990242230744f
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-12-12 23:03:47 +0400

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,64 +12,88 @@ import {Zx9SpeakerComponent} from './components/speakers/zx9-speaker/zx9-speaker
 import {Yx1EarphonesComponent} from './components/earphones/yx1-earphones/yx1-earphones.component'
 import {CheckoutComponent} from './components/checkout/checkout.component'
 
+
 const routes: Routes = [
   {
-    path: "",
-    children: [
+    path:"", component: HomeComponent
+  },
+  {
+    path:"headphones",component:HeadphonesComponent, children:
+    [
       {
-        path:"", component: HomeComponent
+        path:"xx99-mark-two-headphones", component: Xx99MarkTwoHeadphonesComponent
       },
       {
-        path:"headphones/xx99-mark-two-headphones",component:Xx99MarkTwoHeadphonesComponent
+        path:"xx99-mark-one-headphones",component:Xx99MarkOneHeadphonesComponent
       },
       {
-        path:"headphones/xx99-mark-one-headphones",component:Xx99MarkOneHeadphonesComponent
+        path:"xx59-headphones",component:Xx59HeadphonesComponent
       },
+    ]
+  },
+  {
+    path:"earphones",component:EarphonesComponent, children:
+    [
       {
-        path:"headphones/xx59-headphones",component:Xx59HeadphonesComponent
-      },
-      {
-        path:"headphones",component:HeadphonesComponent
-      },
-      {
-        path:"earphones/yx1-earphones",component:Yx1EarphonesComponent
-      },
-      {
-        path:"earphones",component:EarphonesComponent
-      },
-      {
-        path:"speakers/zx9-speaker",component:Zx9SpeakerComponent
-      },
-      {
-        path:"speakers/zx7-speaker",component:Zx7SpeakerComponent
-      },
-      {
-        path:"speakers",component:SpeakersComponent
-      },
-      {
-        path:"checkout",component: CheckoutComponent
+        path:"yx1-earphones",component:Yx1EarphonesComponent
       }
     ]
+  },
+  {
+    path:"speakers",component:SpeakersComponent, children:
+    [
+      {
+        path:"zx9-speaker",component:Zx9SpeakerComponent
+      },
+      {
+        path:"zx7-speaker",component:Zx7SpeakerComponent
+      }
+    ]
+  },
+  {
+    path:"checkout",component: CheckoutComponent
   }
-
-  // {
-  //   path:"", component: HomeComponent
-  // },
-  // {
-  //   path:"headphones",component:HeadphonesComponent, children:
-  //   [
-  //     {
-  //       path:"xx99-mark-two-headphones", component: Xx99MarkTwoHeadphonesComponent
-  //     },
-  //   ]
-  // },
-  // {
-  //   path:"earphones",component:EarphonesComponent
-  // },
-  // {
-  //   path:"speakers",component:SpeakersComponent
-  // },
-];
+]
+// const routes: Routes = [
+//   {
+//     path: "",
+//     children: [
+//       {
+//         path:"", component: HomeComponent
+//       },
+//       {
+//         path:"headphones/xx99-mark-two-headphones",component:Xx99MarkTwoHeadphonesComponent
+//       },
+//       {
+//         path:"headphones/xx99-mark-one-headphones",component:Xx99MarkOneHeadphonesComponent
+//       },
+//       {
+//         path:"headphones/xx59-headphones",component:Xx59HeadphonesComponent
+//       },
+//       {
+//         path:"headphones",component:HeadphonesComponent
+//       },
+//       {
+//         path:"earphones/yx1-earphones",component:Yx1EarphonesComponent
+//       },
+//       {
+//         path:"earphones",component:EarphonesComponent
+//       },
+//       {
+//         path:"speakers/zx9-speaker",component:Zx9SpeakerComponent
+//       },
+//       {
+//         path:"speakers/zx7-speaker",component:Zx7SpeakerComponent
+//       },
+//       {
+//         path:"speakers",component:SpeakersComponent
+//       },
+//       {
+//         path:"checkout",component: CheckoutComponent
+//       }
+//     ]
+//   }
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
