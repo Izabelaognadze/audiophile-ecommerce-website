ce8398c666bf745bd0c5ee10f3a3f322638cdcd2
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2024-01-25 14:30:53 +0400

    update home page

--- src/app/design-system/button.directive.ts
+++ src/app/design-system/button.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostBinding, Input } from '@angular/core';
 import { clsx } from 'clsx';
 
-type ButtonType = 'Button1' | 'Button2' | 'Button3' | 'Button4';
+type ButtonType = 'Button1' | 'white' | 'black' | 'Button3' | 'Button4';
 
 @Directive({
   selector: '[appButton]',
@@ -13,12 +13,13 @@ export class ButtonDirective {
   @HostBinding('class')
   get additionalClasses() {
     return clsx({
-      'uppercase tracking-[1px] duration-300 text-base w-32 rounded text-center':
-        true,
+      '': true,
       'bg-blue-500 hover:opacity-80 text-white font-bold py-2 px-4 border border-blue-700':
         this.variant === 'Button1',
-      'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border-2 border-b-blue-500 hover:border-transparent':
-        this.variant === 'Button2',
+      'uppercase border-[1px] border-black w-40 h-12 font-bold text-[13px] tracking-[1px] hover:bg-black hover:text-white':
+        this.variant === 'white',
+      'bg-black text-white hover:bg-clrbgHover hover:cursor-pointer hover:bg-[#4C4C4C] w-40 h-12 uppercase relative mt-6 mb-[55px] text-[13px] tracking-[1px] md:mt-10 md:mb-16 lg:mt-[55px] lg:mb-0':
+        this.variant === 'black',
       'uppercase opacity-50 tracking-[1px] font-bold text-[13px]':
         this.variant === 'Button3',
       'pb-6 opacity-50 hover:opacity-100 hover:cursor-pointer lg:pb-[38px] capitalize hover:text-clrDarkOrange':
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { BestGearComponent } from '../best-gear/best-gear.component';
+import { ButtonDirective } from '../design-system/button.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, CommonModule, BestGearComponent],
+  imports: [FooterComponent, CommonModule, BestGearComponent, ButtonDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
